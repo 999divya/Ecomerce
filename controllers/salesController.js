9a530fc06b1184488483149ba30b5d16ac5baf4c
999divya <ranidivyadas@gmail.com>
2022-06-27 11:17:34 +0530

    updations done

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -5,26 +5,26 @@ const moment = require('moment')
 
 
 
-exports.salesReport = async(req,res)=>{ //  /sales-report
+exports.salesReport = async (req, res) => { //  /sales-report
   if (req.session.adminLoggedin) {
 
     let today = new Date();
 
     let end = moment(today).format("YYYY/MM/DD");
-  
+
     let start = moment(end).subtract(30, "days").format("YYYY/MM/DD");
-  
+
     let orderSuccess = await order
-      .find({ date: { $gte: start, $lte: end }, status: { $ne: "Cancelled" } })
+      .find({ date: { $gte: start, $lte: end }, status: { $ne: "cancelled" } })
     //   .toArray();
-  
+
     let orderTotal = await order
       .find({ date: { $gte: start, $lte: end } })
     //   .toArray();
     let orderSuccessLength = orderSuccess.length;
     let orderTotalLength = orderTotal.length;
     let orderFailLength = orderTotalLength - orderSuccessLength;
-  
+
     let total = 0;
     let paypal = 0;
     let razorpay = 0;
@@ -35,7 +35,19 @@ exports.salesReport = async(req,res)=>{ //  /sales-report
         cod++;
       }
     }
-  
+    for (let i = 0; i < orderSuccessLength; i++) {
+      total = total + orderSuccess[i].totalAmount;
+      if (orderSuccess[i].paymentMethod == "paypal") {
+        paypal++;
+      }
+    }
+    for (let i = 0; i < orderSuccessLength; i++) {
+      total = total + orderSuccess[i].totalAmount;
+      if (orderSuccess[i].paymentMethod == "Razorpay") {
+        razorpay++;
+      }
+    }
+
     var data = {
       start: start,
       end: end,
@@ -48,31 +60,31 @@ exports.salesReport = async(req,res)=>{ //  /sales-report
       razorpay: razorpay,
       currentOrders: orderSuccess
     }
-  
+
     res.render('adminviews/report', { admin: true, data })
 
-  }else{
+  } else {
     res.redirect('/login')
-}
+  }
 }
 
 
-exports.saveReport = async(req,res)=>{ //   /save-report
+exports.saveReport = async (req, res) => { //   /save-report
   if (req.session.adminLoggedin) {
     let end = moment(req.body.EndDate).format('YYYY/MM/DD')
     let start = moment(req.body.StartDate).format('YYYY/MM/DD')
-  
+
     let orderSuccess = await order
       .find({ date: { $gte: start, $lte: end }, status: { $ne: "Cancelled" } })
     //   .toArray();
-  
+
     let orderTotal = await order
       .find({ date: { $gte: start, $lte: end } })
     //   .toArray();
     let orderSuccessLength = orderSuccess.length;
     let orderTotalLength = orderTotal.length;
     let orderFailLength = orderTotalLength - orderSuccessLength;
-  
+
     let total = 0;
     let paypal = 0;
     let razorpay = 0;
@@ -83,7 +95,7 @@ exports.saveReport = async(req,res)=>{ //   /save-report
         cod++;
       }
     }
-  
+
     var data = {
       start: start,
       end: end,
@@ -96,13 +108,13 @@ exports.saveReport = async(req,res)=>{ //   /save-report
       razorpay: razorpay,
       currentOrders: orderSuccess
     }
-  
-  
+
+
     res.render('adminviews/report', { admin: true, data })
-  }else{
+  } else {
     res.redirect('/login')
+  }
 }
-}
 
 
 
@@ -110,146 +122,147 @@ exports.saveReport = async(req,res)=>{ //   /save-report
 
 
 
-exports.getChartDetails=async(req,res)=>{
+exports.getChartDetails = async (req, res) => {
 
   if (req.session.adminLoggedin) {
-  let dailySale = await order.aggregate([
+    let dailySale = await order.aggregate([
 
-    {
-      $match: {
+      {
+        $match: {
           status: "Deliverd"
-      }
-  },
+        }
+      },
 
-  {
-      $group: {
+      {
+        $group: {
           _id: { $dateToString: { format: "%Y-%m-%d", date: "$dateIso" } },
           totalAmount: { $sum: "$totalAmount" },
           count: { $sum: 1 }
+        }
+      },
+      {
+        $sort: { _id: -1 }
+      },
+      {
+        $limit: 7
       }
-  },
-  {
-      $sort: { _id: -1 }
-  },
-  {
-      $limit: 7
-  }
 
 
-  ])
+    ])
 
-  // res.json(dailySale)
-
-   
-
-
-
-//current day sale
-let currentDate = new Date()
-currentDate = currentDate.toISOString().split('T')[0]
-
-
-let todaySale  = await order.aggregate([
-
-
-  {
-    $match: {
-        status: "Deliverd"
-    }
-},
-{
-    $project: {
-        date: { $dateToString: { format: "%Y-%m-%d", date: "$dateIso" } }, totalAmount: 1
-    }
-},
-{
-    $match: { dateIso: currentDate }
-},
-{
-    $group: {
-        _id: "$dateIso",
-        total: { $sum: "$totalAmount" },
-
-    }
-}
-
-
-
-])
-
-let data = 0
-todaySale.map(val => data = val.total)
-// res.json(data)
+    // res.json(dailySale)
 
 
 
 
 
-//CURRENT YEAR SALE
-let curDate = new Date()
-let currentYear = curDate.getFullYear();
-currentYear = currentYear + ""
+    //current day sale
+    let currentDate = new Date()
+    currentDate = currentDate.toISOString().split('T')[0]
 
 
-let yearlySale = await order.aggregate([
-
-  {
-    $match: {
-        status: "Deliverd"
-    }
-},
-{
-    $project: {
-        dateIso: { $dateToString: { format: "%Y", date: "$dateIso" } }, totalAmount: 1
-    }
-},
-
-{
-    $group: {
-        _id: "$dateIso",
-        total: { $sum: "$totalAmount" },
-
-    }
-}
-
-])
+    let todaySale = await order.aggregate([
 
 
-// res.json(yearlySale)
+      {
+        $match: {
+          status: "Deliverd"
+        }
+      },
+      {
+        $project: {
+          date: { $dateToString: { format: "%Y-%m-%d", date: "$dateIso" } }, totalAmount: 1
+        }
+      },
+      {
+        $match: { dateIso: currentDate }
+      },
+      {
+        $group: {
+          _id: "$dateIso",
+          total: { $sum: "$totalAmount" },
+
+        }
+      }
 
 
 
+    ])
 
-//CURRENT MONTH SALE
+    let data = 0
+    todaySale.map(val => data = val.total)
+    // res.json(data)
 
-let currentmonthSale = await order.aggregate([
-  {
-      $match: {
+
+
+
+
+    //CURRENT YEAR SALE
+    let curDate = new Date()
+    let currentYear = curDate.getFullYear();
+    currentYear = currentYear + ""
+
+
+    let yearlySale = await order.aggregate([
+
+      {
+        $match: {
+          status: "Deliverd"
+        }
+      },
+      {
+        $project: {
+          dateIso: { $dateToString: { format: "%Y", date: "$dateIso" } }, totalAmount: 1
+        }
+      },
+
+      {
+        $group: {
+          _id: "$dateIso",
+          total: { $sum: "$totalAmount" },
+
+        }
+      }
+
+    ])
+
+
+    // res.json(yearlySale)
+
+
+
+
+    //CURRENT MONTH SALE
+
+    let currentmonthSale = await order.aggregate([
+      {
+        $match: {
           "status": "Deliverd"
-      }
-  },
+        }
+      },
 
-  {
-      $group: {
+      {
+        $group: {
           _id: { $dateToString: { format: "%Y-%m", date: '$dateIso' } },
           totalAmount: { $sum: "$totalAmount" },
           count: { $sum: 1 }
-      }
-  },
-  {
-      $sort: { _id: -1 }
-  },
+        }
+      },
+      {
+        $sort: { _id: -1 }
+      },
 
-])
-
-// res.json(currentmonthSale)
+    ])
 
 
-res.json({currentmonthSale, dailySale, data, yearlySale})
 
-  }else{
+
+    res.json({ currentmonthSale, dailySale, data, yearlySale })
+
+
+  } else {
     res.redirect('/login')
-}
+  }
 }
 
 
